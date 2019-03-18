de4f9073dbd54afb42eda442b3d1a1899f4cb8f7
duyu <c_duoyun@outlook.com>
2019-03-18 17:30:01 +0800

    commit

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+$(btn1).on('click', function(){
+    $(images).css({
+        transform:'translateX(0)'
+    })
+})
+$(btn2).on('click', function(){
+    $(images).css({
+        transform:'translateX(-300px)'
+    })
+})
+$(btn3).on('click',function(){
+    $(images).css({
+        transform:'translateX(-600px)'
+    })
+})
